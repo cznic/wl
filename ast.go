6082047a9032dba020bb4c4aae9f55b0b8288d49
cmwslw <cwalker32@gmail.com>
2017-08-08 23:49:34 -0700

    Update testcases, gofmt.

--- ast.go
+++ ast.go
@@ -160,6 +160,7 @@ func (n *ExprList) Pos() token.Pos {
 //	|       PLUS_MINUS Expression                           // Case 67
 //	|       MINUS_PLUS Expression                           // Case 68
 //	|       Expression BACKSLASH Expression                 // Case 69
+//	|       Expression "!=" Expression                      // Case 70
 type Expression struct {
 	Case        int
 	Expression  *Expression
@@ -184,7 +185,7 @@ func (n *Expression) Pos() token.Pos {
 	}
 
 	switch n.Case {
-	case 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 56, 58, 59, 60, 62, 63, 64, 65, 69:
+	case 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 56, 58, 59, 60, 62, 63, 64, 65, 69, 70:
 		return n.Expression.Pos()
 	case 55:
 		return n.Factor.Pos()
